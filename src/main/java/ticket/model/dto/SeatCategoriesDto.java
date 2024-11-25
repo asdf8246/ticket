ef9fae04054aee53b168b9982649e41a880ae79d
user <user@LAPTOP-5JU6MU20>
2024-11-25 16:55:17 +0800

    set

--- src/main/java/ticket/model/dto/SeatCategoriesDto.java
+++ src/main/java/ticket/model/dto/SeatCategoriesDto.java
@@ -2,6 +2,17 @@ package ticket.model.dto;
 
 import lombok.Data;
 
+/**
+ CREATE TABLE seat_categories (
+    seat_category_id INT AUTO_INCREMENT PRIMARY KEY,
+    event_id INT NOT NULL,  -- 外鍵連接到活動
+    category_name VARCHAR(255)  NOT NULL,  -- 座位等級名稱（如：VIP、普通等）
+    seat_price INT NOT NULL,  -- 票價
+    num_seats INT NOT NULL,  -- 該區域有多少個座位
+    FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE -- 外鍵約束 參照 events 的 event_id 欄位
+);
+ */
+
 @Data
 public class SeatCategoriesDto {
 	private Integer seatCategoryId;
--- src/main/java/ticket/model/entity/Seats.java
+++ src/main/java/ticket/model/entity/Seats.java
@@ -9,8 +9,8 @@ import lombok.Data;
     seat_category_id INT NOT NULL,  -- 外鍵連接到座位等級
     seat_number VARCHAR(255) NOT NULL,  -- 座位編號，如 A1、B3 等
     seat_status ENUM('available', 'reserved', 'sold') DEFAULT 'available',  -- 座位狀態
-    FOREIGN KEY (event_id) REFERENCES events(event_id),
-    FOREIGN KEY (seat_category_id) REFERENCES seat_categories(seat_category_id)
+    FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE,
+    FOREIGN KEY (seat_category_id) REFERENCES seat_categories(seat_category_id) ON DELETE CASCADE
 );
  */
 
